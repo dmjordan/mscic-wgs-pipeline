db880f65072679d956afdbe0f9b1613a5614c897
Daniel Jordan <daniel.jordan1@mssm.edu>
2021-05-07 12:16:08 -0400

    cleaned up superfluous files

--- rpy2_diagnostic.py
+++
@@ -1,3 +0,0 @@
-import rpy2
-
-print(rpy2)
--- run_genesis_gwas.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-ml plink
-
-bsub -K < ../../scripts/WGS/lsf_submit_prep_files_for_pcair.sh
-plink --bfile 625_Samples.cohort.QC_filtered.sample_matched.maf_and_ld --make-bed --out 625_Samples.cohort.QC_filtered.sample_matched.maf_and_ld.plink_recoded
-conda run -n r-gwas Rscript --vanilla ../../scripts/WGS/genesis_pca.R
