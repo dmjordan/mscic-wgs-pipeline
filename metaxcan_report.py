6f97f01103bd99f55efe36688ed61bb3f33b4f99
dmjordan <=_#B::.@QJtmJA]z-o%j*]Bj>
2021-07-05 10:43:28 -0400

    Added per-phenotype FDRs to metaxcan report

--- metaxcan_report.py
+++ metaxcan_report.py
@@ -2,12 +2,13 @@ import pandas as pd
 import typing
 import os
 from tqdm import tqdm
+import statsmodels.api as sm
 
 if typing.TYPE_CHECKING:
     from snakemake.script import Snakemake
     snakemake: Snakemake
 
-columns_to_extract = ["gene", "gene_name", "pvalue", "fdr", "bonferroni", "phenotype", "tissue"]
+columns_to_extract = ["gene", "gene_name", "pvalue", "fdr_tissue", "bonferroni_tissue", "phenotype", "tissue"]
 tables = []
 for infile in tqdm(snakemake.input):
     phenotype, tissue, _csv = os.path.basename(infile).split(".")
@@ -16,8 +17,21 @@ for infile in tqdm(snakemake.input):
     table = pd.read_csv(infile, sep=None, engine="python")
     table["phenotype"] = phenotype
     table["tissue"] = tissue
-    table = table.loc[table.fdr < 0.1, columns_to_extract]
+
+    table["fdr_tissue"] = table["fdr"]
+    table["bonferroni_tissue"] = table["bonferroni"]
+    table = table.loc[columns_to_extract]
     tables.append(table)
 
-pd.concat(tables).to_csv(snakemake.output[0], sep="\t", header=True, index=False)
+full_table = pd.concat(tables)
+for phenotype in full_table["phenotype"].unique():
+    phenotype_select = (full_table["phenotype"] == phenotype) & (full_table["tissue"] != "Meta")
+    fdr = sm.stats.multipletests(full_table.loc[phenotype_select, "pvalue"], method='fdr_bh')
+    bonferroni = sm.stats.multipletests(full_table.loc[phenotype_select, "pvalue"], method='bonferroni')
+    full_table.loc[full_table["phenotype"] == phenotype, "fdr_phenotype"] = fdr
+    full_table.loc[full_table["phenotype"] == phenotype, "bonferroni_phenotype"] = bonferroni
+
+full_table.to_csv(snakemake.output[0], sep="\t", header=True, index=False)
+
+
 
