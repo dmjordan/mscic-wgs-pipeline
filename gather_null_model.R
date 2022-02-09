12dcbff82443cc1b111dd598c89edd1bfa0f6469
Daniel Jordan <daniel.jordan1@mssm.edu>
2022-02-09 13:26:24 -0500

    fixed null model gather

--- gather_null_model.R
+++ gather_null_model.R
@@ -6,8 +6,7 @@ models %>% compact("error") %>% map_chr("error") -> error_messages
 models %>% keep(~ is.null(.x$error)) -> models
 models %>% discard("converged") -> failed_models
 models %>% keep("converged") -> succeeded_models
-cat(length(all_covar_combinations), "covar combinations tested in", exec_time[["elapsed"]], "seconds\n")
-cat(length(error_messages), "died with an error;", length(failed_models), "failed to converge;", length(succeeded_models), "succeeded\n")
+cat("of", length(error_messages) + length(models), "models:", length(error_messages), "died with an error;", length(failed_models), "failed to converge;", length(succeeded_models), "succeeded\n")
 cat("error summary:\n")
 print(enframe(error_messages) %>% count(value))
 map_dbl(succeeded_models, "AIC") %>% compact %>% which.min %>% pluck(succeeded_models, .) -> model
